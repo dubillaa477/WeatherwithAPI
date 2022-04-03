943bf3dce4cc092f4060c65d96ad88c8e6546b88
dubil <dubil@DESKTOP-9740NAE>
2022-04-03 10:29:13 -0600

    Add project files.

---
+++ WeatherwithAPI/WeatherwithAPI/Model/WeatherModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherwithAPI.Model
+{
+
+    public class WeatherModel
+    {
+        public Datum[] data { get; set; }
+        public double count { get; set; }
+    }
+
+    public class Datum
+    {
+        public double rh { get; set; }
+        public string pod { get; set; }
+        public float lon { get; set; }
+        public float pres { get; set; }
+        public string timezone { get; set; }
+        public string ob_time { get; set; }
+        public string country_code { get; set; }
+        public double clouds { get; set; }
+        public double ts { get; set; }
+        public double solar_rad { get; set; }
+        public string state_code { get; set; }
+        public string city_name { get; set; }
+        public float wind_spd { get; set; }
+        public string wind_cdir_full { get; set; }
+        public string wind_cdir { get; set; }
+        public float slp { get; set; }
+        public double vis { get; set; }
+        public double h_angle { get; set; }
+        public string sunset { get; set; }
+        public double dni { get; set; }
+        public float dewpt { get; set; }
+        public double snow { get; set; }
+        public double uv { get; set; }
+        public double precip { get; set; }
+        public double wind_dir { get; set; }
+        public string sunrise { get; set; }
+        public double ghi { get; set; }
+        public double dhi { get; set; }
+        public double aqi { get; set; }
+        public float lat { get; set; }
+        public Weather weather { get; set; }
+        public string datetime { get; set; }
+        public float temp { get; set; }
+        public string station { get; set; }
+        public float elev_angle { get; set; }
+        public float app_temp { get; set; }
+    }
+
+    public class Weather
+    {
+        public string icon { get; set; }
+        public double code { get; set; }
+        public string description { get; set; }
+    }
+
+}
---
+++ WeatherwithAPI/WeatherwithAPI/ViewModel/MainPageVM.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherwithAPI.Model;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WeatherwithAPI.ViewModel
+{
+    internal class MainPageVM : INotifyPropertyChanged
+    {
+        private WeatherModel data;
+
+        public WeatherModel Data
+        {
+            get => data;
+            set { data = value; 
+                OnPropertyChanged();    
+            }
+        }
+
+        public Command buscarData { get; set; }
+
+
+        public MainPageVM()
+        {
+            //Use callback functions
+            data = new WeatherModel();
+            buscarData = new Command(async (coordenadas)=> {
+                var entry = coordenadas as string;
+                var data = entry.Split(',');
+                string uri = $"https://api.weatherbit.io/v2.0/current?lat={data[0]}&lon={data[1]}&key=97a9a3fe9a9644a98538cf102a260d5c";
+                await GetData(uri);
+            });       
+        }
+
+        //Call to API
+        private async Task GetData(string url) 
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            var json = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<WeatherModel>(json);
+
+            Data = result;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
